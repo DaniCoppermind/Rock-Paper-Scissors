f9c3e7b3aadf572a21f99ebd2cb4cf10188a6878
Daniel García <workspacedaniel2121@gmail.com>
2023-12-21 15:40:41 -0400

    debug

--- code/code.js
+++ code/code.js
@@ -1,5 +1,5 @@
-const playerChoice = document.querySelectorAll('#playerChoice');
-const span = document.getElementById('winner');
+const playerChoice = document.querySelectorAll("#playerChoice");
+const span = document.getElementById("winner");
 
 let roundCount = 0;
 let playerScore = 0;
@@ -8,69 +8,77 @@ let totalRounds = 0;
 
 // document.getElementById('gameBoard').classList.add('none');
 
-playerChoice.forEach(choice => {
-    choice.addEventListener('click', ()=> {
-        if (totalRounds === 0) {span.textContent = `Submit a Round!`;}
-        if (totalRounds > 0) {
-            let playerSelection = (choice.alt).toLowerCase();
-            let computerSelection = getComputerChoice();
-            const result = playRound(playerSelection, computerSelection);
-
-            showScore();
-            roundCount++;
-
-            if (roundCount >= totalRounds) {
-                showWinner();
-                resetGame();
-            }
-        }
+playerChoice.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    if (totalRounds === 0) {
+      span.textContent = `Submit a Round!`;
     }
-)});
+    if (totalRounds > 0) {
+      let playerSelection = choice.alt.toLowerCase();
+      let computerSelection = getComputerChoice();
+      const result = playRound(playerSelection, computerSelection);
+
+      showScore();
+      roundCount++;
+
+      if (roundCount >= totalRounds) {
+        showWinner();
+        resetGame();
+      }
+    }
+  });
+});
 
 function getComputerChoice() {
-    let choicesComputer = ["rock", "paper", "scissors"];
-    let randomChoice = Math.floor(Math.random() * choicesComputer.length);
-    return choicesComputer[randomChoice];
+  let choicesComputer = ["rock", "paper", "scissors"];
+  let randomChoice = Math.floor(Math.random() * choicesComputer.length);
+  return choicesComputer[randomChoice];
 }
 
 function playRound(playerSelection, computerSelection) {
-    if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')) {
-        playerScore++;
-    } else if (playerSelection === computerSelection) {
-    } else {
-        computerScore++;
-    }
-};
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    playerScore++;
+  } else if (playerSelection === computerSelection) {
+  } else {
+    computerScore++;
+  }
+}
 
 function showScore() {
-    const totalScore = document.querySelectorAll('#score');
+  const totalScore = document.querySelectorAll("#score");
 
-    span.textContent = `Round: ${roundCount + 1}`;
-    totalScore[0].textContent = `Score:${playerScore}`;
-    totalScore[1].textContent = `Score:${computerScore}`;
+  span.textContent = `Round: ${roundCount + 1}`;
+  totalScore[0].textContent = `Score:${playerScore}`;
+  totalScore[1].textContent = `Score:${computerScore}`;
 }
 
 function roundsToPlay(input) {
-    totalRounds = input;
-    console.log(totalRounds)
+  totalRounds = input;
+  console.log(totalRounds);
 }
 
 function showWinner() {
-    if (playerScore > computerScore) {
-        span.textContent = `You Win!`;
-    } else if (computerScore > playerScore) {
-        span.textContent = `You Lose!`;
-    } else {
-        span.textContent = `Tie!`;
-    }
+  if (playerScore > computerScore) {
+    span.textContent = `You Win!`;
+  } else if (computerScore > playerScore) {
+    span.textContent = `You Lose!`;
+  } else {
+    span.textContent = `Tie!`;
+  }
 }
 
 const displayGame = document.getElementById("gameBoard");
 
 const resetGame = () => {
-    displayGame.classList.add('none');
-    alert("Thanks for Playing!");
-    setTimeout(() => {
-        document.body.addEventListener('click', ()=>{ location.reload()}); 
-    }, 2);
-}
+  displayGame.classList.add("none");
+  alert("Thanks for Playing!");
+  setTimeout(() => {
+    document.body.addEventListener("click", () => {
+      location.reload();
+    });
+  }, 2);
+};
--- code/interface.js
+++ code/interface.js
@@ -1,12 +1,12 @@
-const buttonStartGame = document.getElementById('button-start');
+const buttonStartGame = document.getElementById("button-start");
 const displayGame = document.getElementById("gameBoard");
-let elementContainer = 0
+let elementContainer = 0;
 
-buttonStartGame.addEventListener('click', ()=> {
-    if (elementContainer >= 1) {
-        alert("You're Playing");
-    } else {
-        displayGame.classList.remove('none');
-        elementContainer++;
-    }
+buttonStartGame.addEventListener("click", () => {
+  if (elementContainer >= 1) {
+    alert("You're Playing");
+  } else {
+    displayGame.classList.remove("none");
+    elementContainer++;
+  }
 });
